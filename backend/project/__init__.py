281415929bb9d816fa594cfcb1752fae61b2acf5
lfhdz774 <luishernandez101198@hotmail.com>
2024-08-22 21:06:46 -0600

    Connection with Ubuntu Server

--- backend/project/__init__.py
+++ backend/project/__init__.py
@@ -12,14 +12,17 @@ CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 swagger = Swagger(app, template_file='swagger.yaml')
 
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://vanhxpev:ekq-InJ8Vr26kOHoCGhaXRnOmP-VzSiz@bubble.db.elephantsql.com/vanhxpev'
+#app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://vanhxpev:ekq-InJ8Vr26kOHoCGhaXRnOmP-VzSiz@bubble.db.elephantsql.com/vanhxpev'+
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:1163911639@localhost:5432/flask_test'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 Migrate(app,db)
 
 from project.login.view import login_blueprint
-from project.serverAdmin.view import serverAdmin_blueprint
+from project.serverConnection.view import serverConnection_blueprint
+#from project.serverAdmin.view import serverAdmin_blueprint
 
 app.register_blueprint(login_blueprint)
-app.register_blueprint(serverAdmin_blueprint)
+app.register_blueprint(serverConnection_blueprint)
+#app.register_blueprint(serverAdmin_blueprint)

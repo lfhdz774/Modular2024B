41001ff1bc6bdb614666c0cbda97f34ed8cee231
lfhdz774 <luishernandez101198@hotmail.com>
2024-08-26 11:44:57 -0600

    Nova DB

--- backend/project/__init__.py
+++ backend/project/__init__.py
@@ -4,6 +4,10 @@ from flask_migrate import Migrate
 from flasgger import Swagger
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
+import os
+from dotenv import load_dotenv
+# Load environment variables from .flaskenv file
+load_dotenv('.flaskenv')
 
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = 'your-secret-key'  # Change this!
@@ -12,9 +16,11 @@ CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 swagger = Swagger(app, template_file='swagger.yaml')
 
-#app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://vanhxpev:ekq-InJ8Vr26kOHoCGhaXRnOmP-VzSiz@bubble.db.elephantsql.com/vanhxpev'+
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:1163911639@localhost:5432/flask_test'
+PGUSER = os.getenv('PGUSER')
+PGPASSWORD = os.getenv('PGPASSWORD')
+app.config["SQLALCHEMY_DATABASE_URI"] = f'postgresql://{PGUSER}:{PGPASSWORD}@ep-broad-moon-a53iqxig.us-east-2.aws.neon.tech/serverportaldb?options=project%3Dep-broad-moon-a53iqxig&sslmode=require'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 
 db = SQLAlchemy(app)
 Migrate(app,db)

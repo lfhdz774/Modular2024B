93aaf2a81c261a1c6e7be254a19bb993478f763c
lfhdz774 <luishernandez101198@hotmail.com>
2024-11-18 14:01:30 -0600

    Changes

--- backend/server.py
+++ backend/server.py
@@ -9,6 +9,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(ssl_context=('/etc/letsencrypt/live/yourdomain.com/fullchain.pem',
-                         '/etc/letsencrypt/live/yourdomain.com/privkey.pem'),
-            port=5000) 
+    app.run(ssl_context=('certs/fullchain.pem', 'certs/privkey.pem'), host='0.0.0.0', port=5000)

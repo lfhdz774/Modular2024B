e1f822394766924deda2de2df143b0c869526b0f
lfhdz774 <luishernandez101198@hotmail.com>
2024-11-18 13:04:31 -0600

    Changes

--- backend/project/__init__.py
+++ backend/project/__init__.py
@@ -29,7 +29,7 @@ jwt = JWTManager(app)
 
 Session(app)
 
-CORS(app, supports_credentials=True, resources={r"/*": {"origins": ["http://localhost:3000", "http://serverportal-app.org:3000"]}})
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": ["http://localhost:3000","http://serverportal-app.org"]}})
 
 
 swagger = Swagger(app, template_file='swagger.yaml')

a40935075b1f84af75ea48f42d5f89ab9c997e9f
Ruravi <joseruravi@hotmail.com>
2024-11-17 22:11:39 -0600

    Update GenerateAccess.py

--- backend/project/GenerateAccess/GenerateAccess.py
+++ backend/project/GenerateAccess/GenerateAccess.py
@@ -128,7 +128,7 @@ class GenerateAccess:
         # Crear un token JWT con la contraseña en los user_claims
         expires = datetime.timedelta(hours=1)  # El token expirará en 1 hora
         user_claims = {'password': password}
-        token = create_access_token(identity='', expires_delta=expires, user_claims=user_claims)
+        token = create_access_token(identity='', expires_delta=expires, additional_claims=user_claims)
         print('Token generado:', token)
         return token
         
